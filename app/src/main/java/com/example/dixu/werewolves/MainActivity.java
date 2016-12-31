c993f0529e137d913c82ed53629fda1d5a33bcf9
Di Xu <dixu@dixu-macbookair.roam.corp.google.com>
2016-12-30 21:41:28 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/dixu/werewolves/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dixu.werewolves;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStartGame(View view) {
+        Log.i(TAG, "Start Game is clicked!");
+    }
+}
